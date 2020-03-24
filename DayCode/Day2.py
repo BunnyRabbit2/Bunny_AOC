697e2348fa25ff2c8b70aa3f0983f0884fd4d751
Lewis Storey <lewis.storey@bijc.co.uk>
2020-03-24 17:23:28 +0000

    D2P1 solved

--- DayCode/Day2.py
+++ DayCode/Day2.py
@@ -8,12 +8,42 @@ def solve():
 def loadInputs(fileLocation):
     if os.path.exists(fileLocation):
         file = open(fileLocation)
-        return file.read()
+        return file.read().split()
     else:
         print("Day 2 input file does not exist")
 
+def calculateWrapping(l,w,h):
+    side1 = l*w
+    side2 = l*h
+    side3 = w*h
+
+    smallestSide = min(side1,side2,side3)
+
+    surfaceArea = 2*side1 + 2*side2 + 2*side3
+
+    return surfaceArea + smallestSide
+
+def getLwhFromString(stringLwh):
+    values = stringLwh.split('x')
+    l = int(values[0])
+    w = int(values[1])
+    h = int(values[2])
+
+    return (l,w,h)
+
+def calculateTotalSF(inputs):
+    totalSF = 0
+
+    for s in inputs:
+        values = getLwhFromString(s)
+        totalSF += calculateWrapping(values[0],values[1],values[2])
+
+    return (totalSF,0)
+
 def solvePuzzle1(fileLocation):
-    output = (0,0)   
+    inputs = loadInputs(fileLocation)
+
+    output = calculateTotalSF(inputs)
 
     print "Day 2 Puzzle 1 Solution - " + str(output[0])
 
