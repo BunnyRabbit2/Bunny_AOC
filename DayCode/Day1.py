096818bb35bdf904a04c8b47881f7450d5ac76d5
Lewis Storey <lewis.storey@bijc.co.uk>
2020-03-24 15:40:42 +0000

    Initial commit and D1P1 solved

---
+++ DayCode/Day1.py
@@ -0,0 +1,26 @@
+import os
+
+def loadInputs(fileLocation):
+    if os.path.exists(fileLocation):
+        file = open(fileLocation)
+        return file.read()
+    else:
+        print("Day 1 input file does not exist")
+
+def solvePuzzle1(fileLocation):
+    inputs = loadInputs(fileLocation)
+
+    currentFloor = 0
+
+    for c in inputs:
+        if c == '(':
+            currentFloor += 1
+        elif c == ')':
+            currentFloor -= 1
+
+    print "Day 1 Puzle 1 Solution - " + str(currentFloor)
+
+def solvePuzzle2(fileLocation):
+    
+
+    print "Day 1 Puzle 2 Solution - " + " "
---
+++ README.md
@@ -0,0 +1,5 @@
+# BunnyAoC2019 Python version
+
+This program will solve all of the problems presented by the Advent of Code 2019 which can be found at <https://adventofcode.com/>
+
+Upon running the program it will output the answer to each puzzle into the console with a label to identify what puzzle the solution is for.
