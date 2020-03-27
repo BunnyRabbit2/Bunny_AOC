37b893edfe0d2101b83e7c343824549ed87f8a63
Lewis Storey <lewis.storey@bijc.co.uk>
2020-03-27 13:47:45 +0000

    partial D7P1 solve continues

--- DayCode/Day7.py
+++ DayCode/Day7.py
@@ -20,12 +20,9 @@ def is_int(s):
         return False
 
 def createGateList(inputs):
-    GATE_ID = 0
-
     gateList = {}
 
     for c in inputs:
-        inSignal = -1
         inWire = "NIL"
         inWire2 = "NIL"
         gateT = "T0"
@@ -34,11 +31,8 @@ def createGateList(inputs):
         output = c.split("->")[1].strip()
         command = c.split("->")[0].split()
         if len(command) == 1:
-            if is_int(command[0]):
-                inSignal = int(command[0])
-            else:
-                inWire = command[0]
-                gateT = "DIODE"
+            inWire = command[0]
+            gateT = "DIODE"
         elif command[1] == "AND":
             inWire = command[0]
             inWire2 = command[2]
@@ -59,55 +53,82 @@ def createGateList(inputs):
             gateT = command[1]
             gateV = int(command[2])
 
-        gateList[GATE_ID] = {
+        gateList[output] = {
             "type":gateT,
             "value":gateV,
             "inWire1":inWire,
             "inWire2":inWire2,
-            "inSignal":inSignal,
-            "outWire":output,
-            "outSignal":0
+            "outSignal":-1,
         }
-        # gateList[GATE_ID] = (gateT, gateV, inWire, inWire2, inSignal, output, 0)
-        GATE_ID += 1
 
     return gateList
 
-def calculateIn(gate):
-    if gate[0] == "DIODE":
-        return
-    elif gate[1] == "AND":
-        return
-    elif gate[1] == "OR":
-        return
-    elif gate[1] == "NOT":
-        return
-    elif gate[1] == "LSHIFT":
-        return
-    elif gate[1] == "RSHIFT":
+def calculateIn(gate, gateList):
+    gateO = gateList[gate]
+
+    if is_int(gateO["inWire1"]):
+        gateO["outSignal"] = int(gateO["inWire1"])
         return
 
-def calculateOut(gate):
-    if gate[0] == "DIODE":
-        return
-    elif gate[1] == "AND":
-        return
-    elif gate[1] == "OR":
-        return
-    elif gate[1] == "NOT":
-        return
-    elif gate[1] == "LSHIFT":
-        return
-    elif gate[1] == "RSHIFT":
-        return
+    if gateList[gateO["inWire1"]]["outSignal"] == -1:
+        calculateIn(gateO["inWire1"], gateList)
+    if gateO["inWire2"] != "NIL":
+        if gateList[gateO["inWire2"]]["outSignal"] == -1:
+            calculateIn(gateO["inWire2"], gateList)
+
+    out = -1
+
+    if gateO["type"] == "DIODE":
+        out = gateList[gateO["inWire1"]]["outSignal"]
+    elif gateO["type"] == "AND":
+        out = gateList[gateO["inWire1"]]["outSignal"] & gateList[gateO["inWire2"]]["outSignal"]
+    elif gateO["type"] == "OR":
+        out = gateList[gateO["inWire1"]]["outSignal"] | gateList[gateO["inWire2"]]["outSignal"]
+    elif gateO["type"] == "NOT":
+        out = ~gateList[gateO["inWire1"]]["outSignal"]
+    elif gateO["type"] == "LSHIFT":
+        out = gateList[gateO["inWire1"]]["outSignal"] << gateO["value"]
+    elif gateO["type"] == "RSHIFT":
+        out = gateList[gateO["inWire1"]]["outSignal"] >> gateO["value"]
+
+    gateO["outSignal"] = out
+
+def calculateOut(gate, gateList):
+    gateO = gateList[gate]
+
+    if gateList[gateO["inWire1"]]["outSignal"] == -1:
+        calculateIn(gateO["inWire1"], gateList)
+    if gateO["inWire2"] != "NIL":
+        if gateList[gateO["inWire2"]]["outSignal"] == -1:
+            calculateIn(gateO["inWire2"], gateList)
+
+    out = -1
+
+    if gateO["type"] == "DIODE":
+        out = gateList[gateO["inWire1"]]["outSignal"]
+    elif gateO["type"] == "AND":
+        out = gateList[gateO["inWire1"]]["outSignal"] & gateList[gateO["inWire2"]]["outSignal"]
+    elif gateO["type"] == "OR":
+        out = gateList[gateO["inWire1"]]["outSignal"] | gateList[gateO["inWire2"]]["outSignal"]
+    elif gateO["type"] == "NOT":
+        out = ~gateList[gateO["inWire1"]]["outSignal"]
+    elif gateO["type"] == "LSHIFT":
+        out = gateList[gateO["inWire1"]]["outSignal"] << gateO["value"]
+    elif gateO["type"] == "RSHIFT":
+        out = gateList[gateO["inWire1"]]["outSignal"] >> gateO["value"]
+
+    gateO["outSignal"] = out
 
 def solvePuzzle1(fileLocation):
     inputs = loadInputs(fileLocation)
     gateList = createGateList(inputs)
+    wires = {}
 
-    output = (0,0)
+    calculateOut("a", gateList)
 
-    print "Day 7 Puzzle 1 Solution - " + str(output[0])
+    output = gateList["a"]
+
+    print "Day 7 Puzzle 1 Solution - " + str(output["outSignal"])
 
 def solvePuzzle2(fileLocation):
     # inputs = loadInputs(fileLocation)
