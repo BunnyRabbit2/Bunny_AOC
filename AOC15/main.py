c173c53a8a07260e4541de7fe1bf3c42df5c0adb
Lewis Storey <lewis.storey@bijc.co.uk>
2020-04-28 12:08:11 +0100

    Moved all AOC15 code into it's own folder Sorted out debug options and file excludes

--- AOC15/main.py
+++ AOC15/main.py
@@ -1,10 +1,12 @@
-from DayCode import Solving
+from DayCode import Day22
 import SolveAll
+import os
 
 def main():
     print "Starting Advent of Code 2019 program - Python Version"
-    # Solving.solvePuzzle()
-    SolveAll.SolveAll()
+    os.chdir('AOC15')
+    Day22.solve()
+    # SolveAll.SolveAll()
 
 if __name__ == "__main__":
     main()
--- DayCode/Solving.py
+++
@@ -1,4 +0,0 @@
-from DayCode import Day22
-
-def solvePuzzle():
-    Day22.solve()
