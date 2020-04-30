9a06c2cf8d095d8351ae9606c686620c45cbb035
Lewis Storey <lewis.storey@bijc.co.uk>
2020-04-30 14:07:57 +0100

    Solved D24 I love recursion

--- AOC15/main.py
+++ AOC15/main.py
@@ -1,11 +1,11 @@
-from DayCode import Day23
+from DayCode import Day24
 import SolveAll
 import os
 
 def main():
     print "Starting Advent of Code 2019 program - Python Version"
     os.chdir('AOC15')
-    Day23.solve()
+    Day24.solve()
     # SolveAll.SolveAll()
 
 if __name__ == "__main__":
