9b78d19bff56f03a40e0ca39f82ba7a06a3b0edc
Lewis Storey <lewis.storey@bijc.co.uk>
2020-04-30 15:35:26 +0100

    Solved D25P1

--- AOC15/main.py
+++ AOC15/main.py
@@ -1,11 +1,11 @@
-from DayCode import Day24
+from DayCode import Day25
 import SolveAll
 import os
 
 def main():
     print "Starting Advent of Code 2019 program - Python Version"
     os.chdir('AOC15')
-    Day24.solve()
+    Day25.solve()
     # SolveAll.SolveAll()
 
 if __name__ == "__main__":
