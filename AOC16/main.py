19651e75d1d710f9179d6cfa34d2b31f03b5e1ac
Lewis Storey <lewis.storey@bijc.co.uk>
2020-05-01 11:34:41 +0100

    Created files needed for AOC16
    Thank you automation script I wrote

---
+++ AOC16/main.py
@@ -0,0 +1,13 @@
+from Aoc16_DayCode import Day01
+import Aoc16_SolveAll
+import os
+
+def main():
+    print "Starting Advent of Code 2016 program - Python 2.7 Version"
+    os.chdir('AOC16')
+    Day01.solve()
+    # SolveAll.SolveAll()
+
+if __name__ == "__main__":
+    main()
+    
