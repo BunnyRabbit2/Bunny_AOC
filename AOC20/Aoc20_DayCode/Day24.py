b09b79bbc29c6c6e4de3bee8f91f51d853a7fe3e
Lewis Storey <lewis.storey@bijc.co.uk>
2020-12-02 14:27:26 +0000

    Generated AOC20 structure

---
+++ AOC20/Aoc20_DayCode/Day24.py
@@ -0,0 +1,37 @@
+import os
+
+def solve():
+    fileLoc = "inputs/day24.txt"
+    solvePuzzle1(fileLoc)
+    solvePuzzle2(fileLoc)
+
+def loadInputs(fileLocation):
+    if os.path.exists(fileLocation):
+        file = open(fileLocation)
+        f = file.readline().strip()
+        if not f == 'AOC16_INPUT_DAY01':
+            print "Invalid input file header: Day24"
+            return False
+     
+        return file.read()
+    else:
+        print "Day 24 input file does not exist"
+
+def solvePuzzle1(fileLocation):
+    inputs = loadInputs(fileLocation)
+    if inputs == '':
+        return
+
+    output = 0
+
+    print "Day 24 Puzzle 1 Solution - " + str(output)
+
+def solvePuzzle2(fileLocation):
+    inputs = loadInputs(fileLocation)
+    if inputs == '':
+        return
+
+    output = 0
+
+    print "Day 24 Puzzle 2 Solution - " + str(output)
+    
---
+++ AOC20/main.py
@@ -0,0 +1,13 @@
+from Aoc20_DayCode import Day01
+import Aoc20_SolveAll
+import os
+
+def main():
+    print "Starting Advent of Code 2020 program - Python 2.7 Version"
+    os.chdir('AOC20')
+    Day01.solve()
+    # SolveAll.SolveAll()
+
+if __name__ == "__main__":
+    main()
+    
