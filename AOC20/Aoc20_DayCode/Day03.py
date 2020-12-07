e77648ba1e9f22a65729ade39a44672e003d3216
Lewis Storey <lewis.storey@bijc.co.uk>
2020-12-07 14:02:26 +0000

    Solved D3P1 and D3P2

--- AOC20/Aoc20_DayCode/Day03.py
+++ AOC20/Aoc20_DayCode/Day03.py
@@ -13,7 +13,7 @@ def loadInputs(fileLocation):
             print "Invalid input file header: Day03"
             return False
      
-        return file.read()
+        return file.readlines()
     else:
         print "Day 03 input file does not exist"
 
@@ -24,6 +24,10 @@ def solvePuzzle1(fileLocation):
 
     output = 0
 
+    grid = createTreeGrid(inputs)
+
+    output = getTreesHit(grid,3,1)
+
     print "Day 03 Puzzle 1 Solution - " + str(output)
 
 def solvePuzzle2(fileLocation):
@@ -31,7 +35,54 @@ def solvePuzzle2(fileLocation):
     if inputs == '':
         return
 
-    output = 0
+    output = 1
+
+    grid = createTreeGrid(inputs)
+
+    res = []
+    slopes = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+
+    for s in slopes:
+        res.append(getTreesHit(grid,s[0],s[1]))
+
+    for r in res:
+        output *= r
 
     print "Day 03 Puzzle 2 Solution - " + str(output)
     
+def createTreeGrid(input):
+    grid = []
+
+    for l in input:
+        line = []
+        for c in l:
+            if c == '#':
+                line.append(True)
+            elif c == '.':
+                line.append(False)
+        grid.append(line)
+
+    return grid
+
+def getTreesHit(grid,speedx,speedy):
+    output = 0
+    x = 0
+    y = 0
+
+    length = len(grid)
+    width = len(grid[0])
+
+    while True:
+        x += speedx
+        y += speedy
+
+        if x >= width:
+            x -= width
+        
+        if y >= length:
+            break
+
+        if grid[y][x]:
+            output += 1
+
+    return output
--- AOC20/main.py
+++ AOC20/main.py
@@ -1,11 +1,11 @@
-from Aoc20_DayCode import Day02
+from Aoc20_DayCode import Day03
 import Aoc20_SolveAll
 import os
 
 def main():
     print "Starting Advent of Code 2020 program - Python 2.7 Version"
     os.chdir('AOC20')
-    Day02.solve()
+    Day03.solve()
     # SolveAll.SolveAll()
 
 if __name__ == "__main__":
