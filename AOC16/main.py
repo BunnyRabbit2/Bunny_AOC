5effb9f2279863172e16e17499d248d7ef23feed
Lewis Storey <iamlewisstorey@yahoo.com>
2023-12-02 01:55:04 +0000

    Completed AOC16|D02

--- AOC16/main.py
+++ AOC16/main.py
@@ -1,11 +1,11 @@
-from aoc_16_day_code import day_01
+from aoc_16_day_code import day_02
 import aoc_16_solve_all
 import os
 
 def main():
     print("Starting Advent of Code 2016 program - Python 3 Version")
     os.chdir('AOC16')
-    day_01.solve()
+    day_02.solve()
     # solve_all.solve_all()
 
 if __name__ == "__main__":
