8b86059569f50051d4f0f9b69631f1bedd3efa50
Lewis Storey <iamlewisstorey@yahoo.com>
2023-12-02 01:30:59 +0000

    Updated to Python 3 and new dir structure

---
+++ AOC16/aoc_16_day_code/day_19.py
@@ -0,0 +1,37 @@
+import os
+
+def solve():
+    file_loc = "inputs/day19.txt"
+    solve_puzzle_1(file_loc)
+    solve_puzzle_2(file_loc)
+
+def load_inputs(file_location: str):
+    if os.path.exists(file_location):
+        file = open(file_location)
+        f = file.readline().strip()
+        if not f == 'AOC16_INPUT_DAY19':
+            print("Invalid input file header: Day19")
+            return False
+     
+        return file.read()
+    else:
+        print("Day 19 input file does not exist")
+
+def solve_puzzle_1(file_location: str):
+    inputs = load_inputs(file_location)
+    if inputs == '':
+        return
+
+    output = 0
+
+    print(f'Day 19 Puzzle 1 Solution - {output}')
+
+def solve_puzzle_2(file_location: str):
+    inputs = load_inputs(file_location)
+    if inputs == '':
+        return
+
+    output = 0
+
+    print(f'Day_19 Puzzle 2 Solution - {output}')
+    
