5d3d09ed3e0b4a7ef3375ac69c177364c5dd214f
Lewis Storey <iamlewisstorey@yahoo.com>
2023-12-03 22:23:30 +0000

    Completed 23D02

--- AOC23/main.py
+++ AOC23/main.py
@@ -1,11 +1,11 @@
-from aoc_23_day_code import day_01
+from aoc_23_day_code import day_02
 import aoc_23_solve_all
 import os
 
 def main():
     print("Starting Advent of Code 2023 program - Python 2.7 Version")
     os.chdir('AOC23')
-    day_01.solve()
+    day_02.solve()
     # solve_all.solve_all()
 
 if __name__ == "__main__":
