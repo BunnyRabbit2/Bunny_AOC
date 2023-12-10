69557eb2f069df46d2494bd88eeb49c2ecc3f6d4
Lewis Storey <iamlewisstorey@yahoo.com>
2023-12-10 23:44:41 +0000

    Got Y23D05P01 on the first run. Nice

--- AOC23/aoc_23_day_code/day_05.py
+++ AOC23/aoc_23_day_code/day_05.py
@@ -17,12 +17,77 @@ def load_inputs(file_location: str):
     else:
         print("Day 05 input file does not exist")
 
+def gen_almanac(input_sets: list[str]):
+    almanac = {
+        'seed_numbers': [],
+        'run_order': [
+            'seed-to-soil',
+            'soil-to-fertilizer',
+            'fertilizer-to-water',
+            'water-to-light',
+            'light-to-temperature',
+            'temperature-to-humidity',
+            'humidity-to-location'
+        ],
+        'seed-to-soil': {},
+        'soil-to-fertilizer': {},
+        'fertilizer-to-water': {},
+        'water-to-light': {},
+        'light-to-temperature': {},
+        'temperature-to-humidity': {},
+        'humidity-to-location': {}
+    }
+
+    for almanac_set in input_sets:
+        if almanac_set.startswith('seeds'):
+            seed_numbers = almanac_set.split(':')[-1].split()
+            seed_numbers = [int(seed_num) for seed_num in seed_numbers]
+            almanac['seed_numbers'] = seed_numbers
+        else:
+            set_split = almanac_set.split(':\n')
+            almanac_name = set_split[0].split()[0]
+            map_sets = set_split[1].split('\n')
+
+            for map_set in map_sets:
+                map_set_split = map_set.split()
+                destination = int(map_set_split[0])
+                source = int(map_set_split[1])
+                set_range = int(map_set_split[2])
+
+                map_key = (source, source + set_range - 1)
+
+                almanac[almanac_name][map_key] = destination - source
+
+    return almanac
+
+def get_mapped_number(seed_number, almanac, check_type):
+    for range_key in almanac[check_type]:
+        if range_key[0] <= seed_number <=range_key[1]:
+            return seed_number + almanac[check_type][range_key]
+    return seed_number
+
+def run_seed_number(seed_number, almanac):
+    run_order = almanac['run_order']
+
+    num_out = seed_number
+
+    for map_check in run_order:
+        num_out = get_mapped_number(num_out, almanac, map_check)
+
+    return num_out
+
 def solve_puzzle_1(file_location: str):
     inputs = load_inputs(file_location)
     if inputs == '':
         return
 
-    output = 0
+    input_sets = inputs.split('\n\n')
+
+    almanac = gen_almanac(input_sets)
+
+    seed_locations = [run_seed_number(seed_num, almanac) for seed_num in almanac['seed_numbers']]
+
+    output = min(seed_locations)
 
     print(f'Day 05 Puzzle 1 Solution - {output}')
 
