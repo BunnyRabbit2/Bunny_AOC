9cae6bf356ce6367c8acd95426562cf375108c23
Lewis Storey <lewis.storey@bijc.co.uk>
2023-12-11 13:43:24 +0000

    Solved Y23D06 with a little help

--- AOC23/aoc_23_day_code/day_06.py
+++ AOC23/aoc_23_day_code/day_06.py
@@ -1,4 +1,6 @@
 import os
+import re
+import math
 
 def solve():
     file_loc = "inputs/day06.txt"
@@ -17,12 +19,48 @@ def load_inputs(file_location: str):
     else:
         print("Day 06 input file does not exist")
 
+def gen_race_data(input_lines):
+    fetch_numbers = re.compile(r'(\d+)')
+
+    race_times = [int(v) for v in fetch_numbers.findall(input_lines[0])]
+    race_dist = [int(v) for v in fetch_numbers.findall(input_lines[1])]
+
+    race_data = list(zip(race_times, race_dist))
+
+    return race_data
+
+def run_race_check(race_time, race_distance):
+    race_distances = [(i, i * (race_time - i)) for i in range(race_time)]
+
+    record_beaters = [i[0] for i in race_distances if i[1] > race_distance]
+
+    return record_beaters
+
+def gen_race_data_alt(input_lines):
+    fetch_numbers = re.compile(r'(\d+)')
+
+    race_time = ''.join(fetch_numbers.findall(input_lines[0]))
+    race_dist = ''.join(fetch_numbers.findall(input_lines[1]))
+
+    return (int(race_time), int(race_dist))
+
+def run_race_check_alt(race_time, race_distance):
+    min_time = (race_time - math.sqrt(race_time * race_time - 4 * race_distance)) / 2
+    max_time = (race_time + math.sqrt(race_time * race_time - 4 * race_distance)) / 2
+
+    return (math.ceil(min_time), math.floor(max_time))
+
 def solve_puzzle_1(file_location: str):
     inputs = load_inputs(file_location)
     if inputs == '':
         return
+    
+    output = 1
 
-    output = 0
+    race_data = gen_race_data(inputs.splitlines())
+    for race in race_data:
+        record_beaters = run_race_check(race[0], race[1])
+        output *= len(record_beaters)
 
     print(f'Day 06 Puzzle 1 Solution - {output}')
 
@@ -31,7 +69,11 @@ def solve_puzzle_2(file_location: str):
     if inputs == '':
         return
 
-    output = 0
+    race_data = gen_race_data_alt(inputs.splitlines())
+
+    record_beaters = run_race_check_alt(race_data[0], race_data[1])
+
+    output = record_beaters[1] - record_beaters[0] + 1
 
     print(f'Day_06 Puzzle 2 Solution - {output}')
     
--- AOC23/main.py
+++ AOC23/main.py
@@ -1,11 +1,11 @@
-from aoc_23_day_code import day_05
+from aoc_23_day_code import day_06
 import aoc_23_solve_all
 import os
 
 def main():
     print("Starting Advent of Code 2023 program - Python 2.7 Version")
     os.chdir('AOC23')
-    day_05.solve()
+    day_06.solve()
     # solve_all.solve_all()
 
 if __name__ == "__main__":
