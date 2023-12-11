6943706dece2955ff32bfb0120fc9e5007ca0305
Lewis Storey <lewis.storey@bijc.co.uk>
2023-12-11 11:56:44 +0000

    Better solution for Y23D05P02 Checking ranges instead of individual seeds is a far better way of doing things

--- AOC23/aoc_23_day_code/day_05.py
+++ AOC23/aoc_23_day_code/day_05.py
@@ -46,10 +46,17 @@ def gen_almanac(input_sets: list[str]):
 
             almanac['seed_numbers'] = seed_numbers
 
-            seed_num_starts = [int(seed_num) for x, seed_num in enumerate(seed_numbers) if x % 2 == 0]
-            seed_num_ranges = [int(seed_num) for x, seed_num in enumerate(seed_numbers) if x % 2 == 1]
+            seed_num_starts = seed_numbers[::2]
+            seed_num_ranges = seed_numbers[1::2]
 
-            almanac['seed_ranges'] = [(range_start, seed_num_ranges[x]) for x, range_start in enumerate(seed_num_starts)]
+            almanac['seed_ranges'] = list(zip(seed_num_starts, seed_num_ranges))
+
+            new_seed_ranges = []
+
+            for seed_range in almanac['seed_ranges']:
+                new_seed_ranges.append((seed_range[0], seed_range[0] + seed_range[1] - 1))
+            
+            almanac['seed_ranges'] = new_seed_ranges
         else:
             set_split = almanac_set.split(':\n')
             almanac_name = set_split[0].split()[0]
@@ -69,7 +76,7 @@ def gen_almanac(input_sets: list[str]):
 
 def get_mapped_number(seed_number, almanac, check_type):
     for range_key in almanac[check_type]:
-        if range_key[0] <= seed_number <=range_key[1]:
+        if range_key[0] <= seed_number <= range_key[1]:
             return seed_number + almanac[check_type][range_key]
     return seed_number
 
@@ -82,6 +89,37 @@ def run_seed_number(seed_number, almanac):
         num_out = get_mapped_number(num_out, almanac, map_check)
 
     return num_out
+
+def get_mapped_range(seed_range, almanac, check_type):
+
+    check_ranges = almanac[check_type].keys()
+    check_ranges = sorted(check_ranges, key = lambda x: x[0])
+
+    new_seed_ranges = []
+
+    for check_range in check_ranges:
+        if check_range[0] <= seed_range[0] <= check_range[1]:
+            modifier = almanac[check_type][check_range]
+            if seed_range[1] <= check_range[1]:
+                new_seed_ranges.append((seed_range[0] + modifier, seed_range[1] + modifier))
+            else:
+                new_seed_ranges.append((seed_range[0] + modifier, check_range[1] + modifier))
+                new_seed_ranges.extend(get_mapped_range((check_range[1] + 1, seed_range[1]), almanac, check_type))
+
+    return new_seed_ranges
+
+def run_seed_range(seed_range, almanac):
+    run_order = almanac['run_order']
+
+    map_ranges_out = [seed_range]
+
+    for map_check in run_order:
+        next_map_ranges = []
+        for check_range in map_ranges_out:
+            next_map_ranges.extend(get_mapped_range(check_range, almanac, map_check))
+        map_ranges_out = next_map_ranges
+
+    return map_ranges_out
 
 def solve_puzzle_1(file_location: str):
     inputs = load_inputs(file_location)
@@ -107,14 +145,12 @@ def solve_puzzle_2(file_location: str):
 
     almanac = gen_almanac(input_sets)
 
-    seed_locations = []
+    seed_location_ranges = []
 
     for seed_range in almanac['seed_ranges']:
-        seed_start = seed_range[0]
-        for i in range(seed_range[1]):
-            seed_locations.append(run_seed_number(seed_start + i, almanac))
+        seed_location_ranges.extend(run_seed_range(seed_range, almanac))
 
-    output = min(seed_locations)
+    output = min([loc_range[0] for loc_range in seed_location_ranges])
 
     print(f'Day_05 Puzzle 2 Solution - {output}')
     
