c220de7e239d8cc6e55f412d0504a46f116b318f
Lewis Storey <lewis.storey@bijc.co.uk>
2024-12-09 15:56:27 +0000

    Completed AOC24|D02

--- aoc_24/aoc_24_day_code/day_02.py
+++ aoc_24/aoc_24_day_code/day_02.py
@@ -1,5 +1,6 @@
 """Code to solve puzzle for day 02"""
 import os
+from itertools import pairwise, combinations
 
 def solve():
     """Solves the day"""
@@ -20,6 +21,41 @@ def load_inputs(file_location):
     else:
         print("Day 02 input file does not exist")
         return ''
+    
+def is_report_safe(report_nums) -> bool:
+    """Returns is the report handed in is safe
+    
+    Uses the following rules:
+        1. The numbers must always increase or always decrease
+        2. The difference cannot be 0 or larger than three
+    """
+    diffs = [p[0] - p[1] for p in pairwise(report_nums)]
+
+    if 0 in diffs:
+        return False
+    
+    if any([n > 3 or n < -3 for n in diffs]):
+        return False
+    
+    return all([n > 0 for n in diffs]) or all([n < 0 for n in diffs])
+
+def is_report_safe_dampened(report_nums) -> bool:
+    """Returns is the report handed in is safe when problems are dampened
+    
+    Will check each report and then the reports that fail are checked with each single entry removed
+    """
+    if is_report_safe(report_nums):
+        return True
+    
+    possible_indexes = [i for i in range(len(report_nums))]
+
+    possible_combis = combinations(possible_indexes, len(possible_indexes) - 1)
+
+    for combi in possible_combis:
+        if is_report_safe([report_nums[i] for i in combi]):
+            return True
+
+    return False
 
 def solve_puzzle_1(file_location):
     """Solves puzzle 1 for the day"""
@@ -27,7 +63,9 @@ def solve_puzzle_1(file_location):
     if inputs == '':
         print("Day 02 Puzzle 1 - NO INPUTS")
     else:
-        output = 0
+        report_list = [[int(n) for n in l.split()] for l in inputs.split('\n')]
+
+        output = sum([1 if is_report_safe(r) else 0 for r in report_list])
 
         print("Day 02 Puzzle 1 Solution - " + str(output))
 
@@ -37,7 +75,9 @@ def solve_puzzle_2(file_location):
     if inputs == '':
         print("Day 02 Puzzle 2 - NO INPUTS")
     else:
-        output = 0
+        report_list = [[int(n) for n in l.split()] for l in inputs.split('\n')]
+
+        output = sum([1 if is_report_safe_dampened(r) else 0 for r in report_list])
 
         print("Day 02 Puzzle 2 Solution - " + str(output))
     
--- aoc_24/main.py
+++ aoc_24/main.py
@@ -7,7 +7,7 @@ def main():
     """Main entry point for the solving file"""
     print("Starting Advent of Code 2024 program - Python 3 Version")
     os.chdir('aoc_24')
-    day_01.solve()
+    day_02.solve()
     # aoc_24_solve_all.solve_all()
 
 if __name__ == "__main__":
