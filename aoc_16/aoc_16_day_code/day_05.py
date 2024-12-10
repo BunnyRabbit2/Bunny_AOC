5fa7cb8300c9761399944958630c4912abffece3
Lewis Storey <lewis.storey@bijc.co.uk>
2024-12-10 14:51:48 +0000

    Completed AOC16|D05

--- aoc_16/aoc_16_day_code/day_05.py
+++ aoc_16/aoc_16_day_code/day_05.py
@@ -1,5 +1,6 @@
 """Code to solve puzzle for day 05"""
 import os
+from hashlib import md5
 
 def solve():
     """Solves the day"""
@@ -27,7 +28,22 @@ def solve_puzzle_1(file_location):
     if inputs == '':
         print("Day 05 Puzzle 1 - NO INPUTS")
     else:
-        output = 0
+        room_id = inputs
+
+        passcode = ''
+
+        current_index = 1
+
+        while (len(passcode) < 8):
+            check_hash = md5(f'{room_id}{current_index}'.encode())
+
+            check_hash = check_hash.hexdigest()
+
+            if check_hash[:5] == '00000':
+                passcode += check_hash[5]
+            current_index += 1
+        
+        output = passcode
 
         print("Day 05 Puzzle 1 Solution - " + str(output))
 
@@ -37,7 +53,28 @@ def solve_puzzle_2(file_location):
     if inputs == '':
         print("Day 05 Puzzle 2 - NO INPUTS")
     else:
-        output = 0
+        room_id = inputs
+
+        passcode = {}
+
+        current_index = 1
+
+        while (len(passcode) < 8):
+            check_hash = md5(f'{room_id}{current_index}'.encode())
+
+            check_hash = check_hash.hexdigest()
+
+            if check_hash[:5] == '00000':
+                pos = check_hash[5]
+
+                if pos.isdigit():
+                    pos = int(pos)
+                    if pos not in passcode and 0 <= pos <= 7:
+                        passcode[pos] = check_hash[6]
+
+            current_index += 1
+
+        output = ''.join([passcode[i] for i in range(8)])
 
         print("Day 05 Puzzle 2 Solution - " + str(output))
     
--- aoc_16/main.py
+++ aoc_16/main.py
@@ -7,7 +7,7 @@ def main():
     """Main entry point for the solving file"""
     print("Starting Advent of Code 2016 program - Python 3 Version")
     os.chdir('aoc_16')
-    day_04.solve()
+    day_05.solve()
     # aoc_16_solve_all.solve_all()
 
 if __name__ == "__main__":
