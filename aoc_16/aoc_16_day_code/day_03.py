f5f452acd6c35c804601365e4c1f1ee6e5142417
Lewis Storey <lewis.storey@bijc.co.uk>
2024-12-10 13:22:32 +0000

    Completed AOC16|D03

--- aoc_16/aoc_16_day_code/day_03.py
+++ aoc_16/aoc_16_day_code/day_03.py
@@ -1,11 +1,13 @@
 """Code to solve puzzle for day 03"""
 import os
 
+
 def solve():
     """Solves the day"""
     file_loc = "inputs/day03.txt"
     solve_puzzle_1(file_loc)
     solve_puzzle_2(file_loc)
+
 
 def load_inputs(file_location):
     """Loads the input file"""
@@ -15,11 +17,19 @@ def load_inputs(file_location):
         if not first_line == 'AOC16_INPUT_DAY03':
             print("Invalid input file header: Day03")
             return ''
-     
+
         return input_file.read()
     else:
         print("Day 03 input file does not exist")
         return ''
+
+
+def is_good_triangle(tri_points):
+    if len(tri_points) != 3:
+        return False
+
+    return tri_points[0] + tri_points[1] > tri_points[2] and tri_points[1] + tri_points[2] > tri_points[0] and tri_points[2] + tri_points[0] > tri_points[1]
+
 
 def solve_puzzle_1(file_location):
     """Solves puzzle 1 for the day"""
@@ -27,9 +37,12 @@ def solve_puzzle_1(file_location):
     if inputs == '':
         print("Day 03 Puzzle 1 - NO INPUTS")
     else:
-        output = 0
+        triangles = [[int(n) for n in l.split()] for l in inputs.split('\n')]
+
+        output = sum([1 if is_good_triangle(t) else 0 for t in triangles])
 
         print("Day 03 Puzzle 1 Solution - " + str(output))
+
 
 def solve_puzzle_2(file_location):
     """Solves puzzle 2 for the day"""
@@ -37,7 +50,14 @@ def solve_puzzle_2(file_location):
     if inputs == '':
         print("Day 03 Puzzle 2 - NO INPUTS")
     else:
-        output = 0
+        data_rows = [[int(n) for n in l.split()] for l in inputs.split('\n')]
+
+        triangles = []
+
+        for i in range(0, len(data_rows), 3):
+            for t in range(3):
+                triangles.append((data_rows[i][t], data_rows[i+1][t], data_rows[i+2][t]))
+
+        output = sum([1 if is_good_triangle(t) else 0 for t in triangles])
 
         print("Day 03 Puzzle 2 Solution - " + str(output))
-    
