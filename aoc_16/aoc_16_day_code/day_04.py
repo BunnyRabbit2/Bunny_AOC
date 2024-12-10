561bb10c262027075077f89266da93dc0c1809fd
Lewis Storey <lewis.storey@bijc.co.uk>
2024-12-10 14:32:50 +0000

    Completed AOC24|D04

--- aoc_16/aoc_16_day_code/day_04.py
+++ aoc_16/aoc_16_day_code/day_04.py
@@ -1,5 +1,12 @@
 """Code to solve puzzle for day 04"""
 import os
+import errno
+
+try:
+    os.makedirs("output/y16d04/")
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
 
 def solve():
     """Solves the day"""
@@ -20,6 +27,66 @@ def load_inputs(file_location):
     else:
         print("Day 04 input file does not exist")
         return ''
+def generate_rooms_information(room_list: list[str]):
+    rooms_info = []
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    for room in room_list:
+        room_name, rest = room.rsplit('-',1)
+
+        sector_id, checksum = rest.split('[')
+
+        sector_id = int(sector_id)
+
+        checksum = checksum.replace(']','')
+
+        letters_contained = {c for c in room_name.replace('-','')}
+
+        letter_count = [(c,room_name.count(c)) for c in letters_contained]
+
+        letter_count = sorted(letter_count, key = lambda l: (-l[1], l[0]))
+
+        decrypted_room_name = ''
+
+        for c in room_name:
+            if c in alphabet:
+                decrypted_room_name += alphabet[(alphabet.find(c) + sector_id) % 26]
+            else:
+                decrypted_room_name += c
+
+        rooms_info.append({
+            'room_name': room_name,
+            'decrypted_room_name': decrypted_room_name,
+            'sector_id': sector_id,
+            'checksum': checksum,
+            'letter_count': letter_count
+        })
+
+    return rooms_info
+
+def get_test_rooms():
+    return [
+        'aaaaa-bbb-z-y-x-123[abxyz]',
+        'a-b-c-d-e-f-g-h-987[abcde]',
+        'not-a-real-room-404[oarel]',
+        'totally-real-room-200[decoy]'
+    ]
+
+def write_room_names(rooms):
+    file_path_out = 'output/y16d04/output_room_names.txt'
+    os.makedirs(os.path.dirname(file_path_out), exist_ok=True)
+    if os.path.exists(file_path_out):
+        os.remove(file_path_out)
+    file = open(file_path_out, "a+")
+    for r in rooms:
+        file.write(f'{r['sector_id']} | {r['decrypted_room_name']}\n')
+    file.close()
+
+def is_real_room(room_info):
+    generated_checksum = ''.join([c[0] for c in room_info['letter_count'][0:5]])
+
+    return generated_checksum == room_info['checksum']
 
 def solve_puzzle_1(file_location):
     """Solves puzzle 1 for the day"""
@@ -27,7 +94,9 @@ def solve_puzzle_1(file_location):
     if inputs == '':
         print("Day 04 Puzzle 1 - NO INPUTS")
     else:
-        output = 0
+        rooms_info = generate_rooms_information(inputs.split('\n'))
+        
+        output = sum([r['sector_id'] if is_real_room(r) else 0 for r in rooms_info])
 
         print("Day 04 Puzzle 1 Solution - " + str(output))
 
@@ -37,7 +106,13 @@ def solve_puzzle_2(file_location):
     if inputs == '':
         print("Day 04 Puzzle 2 - NO INPUTS")
     else:
-        output = 0
+        rooms_info = generate_rooms_information(inputs.split('\n'))
+
+        valid_rooms = [r for r in rooms_info if is_real_room(r)]
+
+        write_room_names(valid_rooms)
+
+        output = [r['sector_id'] for r in valid_rooms if 'north' in r['decrypted_room_name']][0]
 
         print("Day 04 Puzzle 2 Solution - " + str(output))
     
