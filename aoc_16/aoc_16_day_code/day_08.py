c1732ab324a1dc958c6928880b6fe2c8cdd64811
Lewis Storey <iamlewisstorey@yahoo.com>
2025-05-17 23:59:05 +0100

    Partial solve of AOC24|D08

--- aoc_16/aoc_16_day_code/day_08.py
+++ aoc_16/aoc_16_day_code/day_08.py
@@ -20,14 +20,35 @@ def load_inputs(file_location):
     else:
         print("Day 08 input file does not exist")
         return ''
-
+    
 def solve_puzzle_1(file_location):
     """Solves puzzle 1 for the day"""
     inputs = load_inputs(file_location)
     if inputs == '':
         print("Day 08 Puzzle 1 - NO INPUTS")
     else:
+        inputs = inputs.split('\n')
+
         output = 0
+
+        light_panel = [['.' for x in range(50)] for y in range(6)]
+
+        for l in inputs:
+            ins = l.split(' ')
+
+            if ins[0] == 'rect':
+                xv, yv = [int(v) for v in ins[1].split('x')]
+
+                for y in range(yv):
+                    for x in range(xv):
+                        light_panel[y][x] = '#'
+            else:
+                move_num = int(ins[2].split('=')[-1])
+                if ins[1] == 'row':
+                    pass
+
+        for y in light_panel:
+            print(''.join(y))
 
         print("Day 08 Puzzle 1 Solution - " + str(output))
 
