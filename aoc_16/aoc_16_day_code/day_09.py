f9613b1afc518a201ec802c2a2598b440f3c91d8
Lewis Storey <iamlewisstorey@yahoo.com>
2025-07-08 22:03:37 +0100

    Solved AOC16D9P1

--- aoc_16/aoc_16_day_code/day_09.py
+++ aoc_16/aoc_16_day_code/day_09.py
@@ -1,5 +1,6 @@
 """Code to solve puzzle for day 09"""
 import os
+import re
 
 def solve():
     """Solves the day"""
@@ -20,6 +21,69 @@ def load_inputs(file_location):
     else:
         print("Day 09 input file does not exist")
         return ''
+    
+def decode_string(encoded_str):
+    decoded_string = ''
+
+    step = 0
+    while step < len(encoded_str):
+        step_amount = 1
+
+        next_char = encoded_str[step]
+
+        if next_char == '(':
+            command = ''
+
+            command_step = 1
+            command_char = encoded_str[step + command_step]
+
+            while command_char != ')':
+                command += command_char
+                command_step += 1
+                command_char = encoded_str[step + command_step]
+
+            r, m = [int(v) for v in command.split('x')]
+
+            copy_step = step + command_step + 1
+
+            copy_slice = encoded_str[copy_step:copy_step + r]
+
+            decoded_string += ''.join([copy_slice for c in range(m)])
+
+            step_amount = len(command) + 2 + r
+        else:
+            decoded_string += next_char
+        
+        step += step_amount
+
+    return decoded_string
+
+def get_decoded_string_len(encoded_str):
+    if '(' in encoded_str and encoded_str[0] != '(':
+        c = '('
+
+    decoded_len = 0
+
+    command_search = re.compile(r'\(\d*x\d*\)')
+
+    next_command = command_search.search(encoded_str)
+
+    if next_command is not None:
+        next_command = next_command.group()
+
+        command_len = len(next_command)
+
+        r, m = [int(v) for v in re.sub('\(|\)', '', next_command).split('x')]
+
+        m_string = encoded_str[command_len:command_len + r]
+
+        decoded_len += (r * m)
+
+        decoded_len += get_decoded_string_len(m_string)
+
+        decoded_len += get_decoded_string_len(encoded_str[command_len + r:])
+
+    return decoded_len
 
 def solve_puzzle_1(file_location):
     """Solves puzzle 1 for the day"""
@@ -27,7 +91,9 @@ def solve_puzzle_1(file_location):
     if inputs == '':
         print("Day 09 Puzzle 1 - NO INPUTS")
     else:
-        output = 0
+        # output = len(decode_string(inputs))
+
+        output = get_decoded_string_len(inputs)
 
         print("Day 09 Puzzle 1 Solution - " + str(output))
 
